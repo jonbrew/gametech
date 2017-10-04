637a857e1c64f4e42913f322847c25f7cab88529
Jonathan Brewer <jonbrew@cs.utexas.edu>
2017-10-04 15:10:24 -0500

    initial commit

---
+++ BaseApplication.cpp
@@ -0,0 +1,334 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    BaseApplication.cpp
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/
+Tutorial Framework (for Ogre 1.9)
+http://www.ogre3d.org/wiki/
+-----------------------------------------------------------------------------
+*/
+
+#include "BaseApplication.h"
+
+#include <OgreTextureManager.h>
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+#include <macUtils.h>
+#endif
+
+//---------------------------------------------------------------------------
+BaseApplication::BaseApplication(void)
+    : mRoot(0),
+    mCamera(0),
+    mSceneMgr(0),
+    mWindow(0),
+    mResourcesCfg(Ogre::StringUtil::BLANK),
+    mPluginsCfg(Ogre::StringUtil::BLANK),
+    mCursorWasVisible(false),
+    mShutDown(false),
+    mInputManager(0),
+    mMouse(0),
+    mKeyboard(0),
+    mOverlaySystem(0),
+    speed(0),
+    direction(0,0,0)
+{
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+    m_ResourcePath = Ogre::macBundlePath() + "/Contents/Resources/";
+#else
+    m_ResourcePath = "";
+#endif
+}
+//---------------------------------------------------------------------------
+BaseApplication::~BaseApplication(void)
+{
+  if (mOverlaySystem) delete mOverlaySystem;
+
+  // Remove ourself as a Window listener
+  Ogre::WindowEventUtilities::removeWindowEventListener(mWindow, this);
+  windowClosed(mWindow);
+  delete mRoot;
+}
+
+//---------------------------------------------------------------------------
+bool BaseApplication::configure(void)
+{
+    // Show the configuration dialog and initialise the system.
+    // You can skip this and use root.restoreConfig() to load configuration
+    // settings if you were sure there are valid ones saved in ogre.cfg.
+    if(mRoot->showConfigDialog())
+    {
+        // If returned true, user clicked OK so initialise.
+        // Here we choose to let the system create a default rendering window by passing 'true'.
+        mWindow = mRoot->initialise(true, "TutorialApplication Render Window");
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+//---------------------------------------------------------------------------
+void BaseApplication::chooseSceneManager(void)
+{
+    // Get the SceneManager, in this case a generic one
+    mSceneMgr = mRoot->createSceneManager(Ogre::ST_GENERIC);
+
+    // Initialize the OverlaySystem (changed for Ogre 1.9)
+    mOverlaySystem = new Ogre::OverlaySystem();
+    mSceneMgr->addRenderQueueListener(mOverlaySystem);
+}
+//---------------------------------------------------------------------------
+void BaseApplication::createCamera(void)
+{
+    // Create the camera
+    mCamera = mSceneMgr->createCamera("PlayerCam");
+
+    // Position it at 500 in Z direction
+    mCamera->setPosition(Ogre::Vector3(0,-50,-80));
+    // Look back along -Z
+    mCamera->lookAt(Ogre::Vector3(0,-50,0));
+    mCamera->setNearClipDistance(5);
+    mCamera->setFOVy(Ogre::Radian(Ogre::Degree(90)));
+}
+//---------------------------------------------------------------------------
+void BaseApplication::createFrameListener(void)
+{
+    Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
+    OIS::ParamList pl;
+    size_t windowHnd = 0;
+    std::ostringstream windowHndStr;
+
+    mWindow->getCustomAttribute("WINDOW", &windowHnd);
+    windowHndStr << windowHnd;
+    pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+
+    mInputManager = OIS::InputManager::createInputSystem(pl);
+
+    mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, true));
+    mMouse = static_cast<OIS::Mouse*>(mInputManager->createInputObject(OIS::OISMouse, true));
+
+    mMouse->setEventCallback(this);
+    mKeyboard->setEventCallback(this);
+
+    // Set initial mouse clipping size
+    windowResized(mWindow);
+
+    // Register as a Window listener
+    Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
+
+    mRoot->addFrameListener(this);
+}
+//---------------------------------------------------------------------------
+void BaseApplication::destroyScene(void)
+{
+}
+//---------------------------------------------------------------------------
+void BaseApplication::createViewports(void)
+{
+    // Create one viewport, entire window
+    Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+    vp->setBackgroundColour(Ogre::ColourValue(0,0,0));
+
+    // Alter the camera aspect ratio to match the viewport
+    mCamera->setAspectRatio(Ogre::Real(vp->getActualWidth()) / Ogre::Real(vp->getActualHeight()));
+}
+//---------------------------------------------------------------------------
+void BaseApplication::setupResources(void)
+{
+    // Load resource paths from config file
+    Ogre::ConfigFile cf;
+    cf.load(mResourcesCfg);
+
+    // Go through all sections & settings in the file
+    Ogre::ConfigFile::SectionIterator seci = cf.getSectionIterator();
+
+    Ogre::String secName, typeName, archName;
+    while (seci.hasMoreElements())
+    {
+        secName = seci.peekNextKey();
+        Ogre::ConfigFile::SettingsMultiMap *settings = seci.getNext();
+        Ogre::ConfigFile::SettingsMultiMap::iterator i;
+        for (i = settings->begin(); i != settings->end(); ++i)
+        {
+            typeName = i->first;
+            archName = i->second;
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+            // OS X does not set the working directory relative to the app.
+            // In order to make things portable on OS X we need to provide
+            // the loading with it's own bundle path location.
+            if (!Ogre::StringUtil::startsWith(archName, "/", false)) // only adjust relative directories
+                archName = Ogre::String(Ogre::macBundlePath() + "/" + archName);
+#endif
+
+            Ogre::ResourceGroupManager::getSingleton().addResourceLocation(
+                archName, typeName, secName);
+        }
+    }
+}
+//---------------------------------------------------------------------------
+void BaseApplication::createResourceListener(void)
+{
+}
+//---------------------------------------------------------------------------
+void BaseApplication::loadResources(void)
+{
+    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+}
+//---------------------------------------------------------------------------
+void BaseApplication::go(void)
+{
+#ifdef _DEBUG
+#ifndef OGRE_STATIC_LIB
+    mResourcesCfg = m_ResourcePath + "resources_d.cfg";
+    mPluginsCfg = m_ResourcePath + "plugins_d.cfg";
+#else
+    mResourcesCfg = "resources_d.cfg";
+    mPluginsCfg = "plugins_d.cfg";
+#endif
+#else
+#ifndef OGRE_STATIC_LIB
+    mResourcesCfg = m_ResourcePath + "resources.cfg";
+    mPluginsCfg = m_ResourcePath + "plugins.cfg";
+#else
+    mResourcesCfg = "resources.cfg";
+    mPluginsCfg = "plugins.cfg";
+#endif
+#endif
+
+    if (!setup())
+        return;
+
+    mRoot->startRendering();
+
+    // Clean up
+    destroyScene();
+}
+//---------------------------------------------------------------------------
+bool BaseApplication::setup(void)
+{
+    mRoot = new Ogre::Root(mPluginsCfg);
+
+    setupResources();
+
+    bool carryOn = configure();
+    if (!carryOn) return false;
+
+    chooseSceneManager();
+    createCamera();
+    createViewports();
+
+    // Set default mipmap level (NB some APIs ignore this)
+    Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
+
+    // Create any resource listeners (for loading screens)
+    createResourceListener();
+    // Load resources
+    loadResources();
+
+    // Create the scene
+    createScene();
+
+    createFrameListener();
+
+    std::srand(std::time(0));
+    speed = (std::rand() % 7) + 4.5;
+    direction = Ogre::Vector3((std::rand() % 25) + -15, (std::rand() % 25) + -15, (std::rand() % 25) + -15);
+    
+    return true;
+};
+//---------------------------------------------------------------------------
+bool BaseApplication::frameRenderingQueued(const Ogre::FrameEvent& evt)
+{
+    if(mWindow->isClosed())
+        return false;
+
+    if(mShutDown)
+        return false;
+
+    // Need to capture/update each device
+    mKeyboard->capture();
+    mMouse->capture();
+
+    // Update ball
+    Ogre::Node* ballNode = mSceneMgr->getRootSceneNode()->getChild("ball");
+    Ogre::Vector3 ballPosition = ballNode->getPosition();
+    int bounds = 100;
+    if(ballPosition.x > bounds || ballPosition.x < -bounds)
+        direction.x *= -1;
+    if(ballPosition.y > bounds || ballPosition.y < -bounds)
+        direction.y *= -1;
+    if(ballPosition.z > bounds || ballPosition.z < -bounds)
+        direction.z *= -1;
+    ballNode->translate(speed * evt.timeSinceLastFrame * direction);
+    mCamera->lookAt(ballPosition);
+    return true;
+}
+//---------------------------------------------------------------------------
+bool BaseApplication::keyPressed( const OIS::KeyEvent &arg )
+{
+  if (arg.key == OIS::KC_ESCAPE) {
+    mShutDown = true;
+  }
+
+  return true;
+}
+//---------------------------------------------------------------------------
+bool BaseApplication::keyReleased(const OIS::KeyEvent &arg)
+{
+    return true;
+}
+//---------------------------------------------------------------------------
+bool BaseApplication::mouseMoved(const OIS::MouseEvent &arg)
+{
+    return true;
+}
+//---------------------------------------------------------------------------
+bool BaseApplication::mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
+{
+    return true;
+}
+//---------------------------------------------------------------------------
+bool BaseApplication::mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
+{
+    return true;
+}
+//---------------------------------------------------------------------------
+// Adjust mouse clipping area
+void BaseApplication::windowResized(Ogre::RenderWindow* rw)
+{
+    unsigned int width, height, depth;
+    int left, top;
+    rw->getMetrics(width, height, depth, left, top);
+
+    const OIS::MouseState &ms = mMouse->getMouseState();
+    ms.width = width;
+    ms.height = height;
+}
+//---------------------------------------------------------------------------
+// Unattach OIS before window shutdown (very important under Linux)
+void BaseApplication::windowClosed(Ogre::RenderWindow* rw)
+{
+    // Only close for window that created OIS (the main window in these demos)
+    if(rw == mWindow)
+    {
+        if(mInputManager)
+        {
+            mInputManager->destroyInputObject(mMouse);
+            mInputManager->destroyInputObject(mKeyboard);
+
+            OIS::InputManager::destroyInputSystem(mInputManager);
+            mInputManager = 0;
+        }
+    }
+}
+//---------------------------------------------------------------------------
---
+++ TutorialApplication.cpp
@@ -0,0 +1,131 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.cpp
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/
+Tutorial Framework (for Ogre 1.9)
+http://www.ogre3d.org/wiki/
+-----------------------------------------------------------------------------
+*/
+
+#include "TutorialApplication.h"
+#include <OgreMeshManager.h>
+
+//---------------------------------------------------------------------------
+TutorialApplication::TutorialApplication(void)
+{
+}
+//---------------------------------------------------------------------------
+TutorialApplication::~TutorialApplication(void)
+{
+}
+
+//---------------------------------------------------------------------------
+void TutorialApplication::createScene(void)
+{
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(.35, .35, .35));
+    Ogre::Light* light = mSceneMgr->createLight("MainLight");
+    light->setPosition(0, 75, 0);
+
+    createWalls(200);
+    Ogre::Entity* ballEntity = mSceneMgr->createEntity("sphere.mesh"); 
+    //ball->setMaterialName("BallColor/CubeMap"); 
+    ballEntity->setCastShadows(true);
+    Ogre::SceneNode* ballNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("ball"); 
+    ballNode->attachObject(ballEntity);
+    ballNode->scale(0.1,0.1,0.1); 
+    ballNode->setPosition(0,0,0);
+
+}
+
+void TutorialApplication::createWalls(int wallSize)
+{
+    // Create plane and mesh
+    Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+    Ogre::MeshManager::getSingleton().createPlane(
+        "planeMesh",
+        Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+        plane, 
+        wallSize, wallSize, 20, 20, 
+        true, 
+        1, 5, 5, 
+        Ogre::Vector3::UNIT_Z);
+    Ogre::Entity* groundEntity = mSceneMgr->createEntity("planeMesh");
+    groundEntity->setCastShadows(false);
+    groundEntity->setMaterialName("Examples/Rockwall");
+    
+    // Create SceneNodes
+    Ogre::SceneNode *groundNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    Ogre::SceneNode *ceilingNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    Ogre::SceneNode *rightNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    Ogre::SceneNode *leftNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    Ogre::SceneNode *frontNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    Ogre::SceneNode *backNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    
+    // Attach entities to Nodes and move to position
+    groundNode->attachObject(groundEntity);
+    groundNode->translate(Ogre::Vector3(0,-wallSize/2,0));
+    ceilingNode->attachObject(groundEntity->clone("ceilingEntity"));
+    ceilingNode->roll(Ogre::Radian(Ogre::Degree(180)));
+    ceilingNode->translate(Ogre::Vector3(0,wallSize/2,0));
+    rightNode->attachObject(groundEntity->clone("rightEntity"));
+    rightNode->roll(Ogre::Radian(Ogre::Degree(-90)));
+    rightNode->yaw(Ogre::Radian(Ogre::Degree(90)));
+    rightNode->translate(Ogre::Vector3(-wallSize/2,0,0));
+    leftNode->attachObject(groundEntity->clone("leftEntity"));
+    leftNode->roll(Ogre::Radian(Ogre::Degree(90)));
+    leftNode->yaw(Ogre::Radian(Ogre::Degree(90)));
+    leftNode->translate(Ogre::Vector3(wallSize/2,0,0));
+    frontNode->attachObject(groundEntity->clone("frontEntity"));
+    frontNode->pitch(Ogre::Radian(Ogre::Degree(-90)));
+    frontNode->translate(Ogre::Vector3(0,0,wallSize/2));
+    backNode->attachObject(groundEntity->clone("backEntity"));
+    backNode->pitch(Ogre::Radian(Ogre::Degree(90)));
+    backNode->translate(Ogre::Vector3(0,0,-wallSize/2));
+}
+//---------------------------------------------------------------------------
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+        // Create application object
+        TutorialApplication app;
+
+        try {
+            app.go();
+        } catch(Ogre::Exception& e)  {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+            std::cerr << "An exception has occurred: " <<
+                e.getFullDescription().c_str() << std::endl;
+#endif
+        }
+
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+//---------------------------------------------------------------------------
